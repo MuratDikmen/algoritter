d42e2f56dcb111095c53bddeeb475409ec68e7c1
Murat Dikmen <mrtdkmn@gmail.com>
2019-11-19 14:35:05 -0500

    feat: recognize tweets and retweets

--- getTweets.js
+++ getTweets.js
@@ -1,14 +1,33 @@
-console.log("test");
-
-var mutationObserver = new MutationObserver(function(mutations) {
-  mutations.forEach(function(mutation) {
-    if (mutation.addedNodes.length == 1) {
-      if (mutation.addedNodes[0].nodeName === "DIV") {
-        console.log(mutation.addedNodes);
+const mutationObserver = new MutationObserver(mutations => {
+  mutations.forEach(mutation => {
+    if (
+      mutation.addedNodes.length == 1 &&
+      mutation.addedNodes[0].nodeName === "DIV"
+    ) {
+      if (
+        mutation.addedNodes[0].childNodes[0].textContent &&
+        mutation.addedNodes[0].childNodes[0].textContent.includes("Retweeted")
+      ) {
+        mutation.addedNodes[0].style.backgroundColor = "green"; // Recognize tweet.
+        console.log(
+          `This is a retweet. For this one, we will use the retweeted tweet's id to keep track of this tweet in the UI. 
+          However, when calculating the position, we will use the main id_str. 
+          Here is the retweet message: ${mutation.addedNodes[0].childNodes[0].textContent}`
+        );
+      } else if (
+        mutation.addedNodes[0].childNodes[0].childNodes[0].childNodes[0]
+          .childNodes[1].childNodes[1].childNodes[0].childNodes[0].childNodes[2]
+          .href
+      ) {
+        mutation.addedNodes[0].style.backgroundColor = "green"; // Recognize tweet.
+        console.log(
+          `This is an original tweet. This is straight forward. 
+          Here is the string where we\'ll find the id_str: ${mutation.addedNodes[0].childNodes[0].childNodes[0].childNodes[0].childNodes[1].childNodes[1].childNodes[0].childNodes[0].childNodes[2].href}`
+        );
+      } else {
+        console.log("false");
       }
-      //   console.log(mutation.addedNodes);
     }
-    // console.log(mutation.addedNodes);
   });
 });
 
@@ -28,10 +47,6 @@ function paintTweets() {
   console.log(articleArr.length);
   console.log(articleArr);
   articleArr[0].style.backgroundColor = "blue";
-  //   articleArr.forEach(article => {
-  //     article.style.backgroundColor = "yellow";
-  //     article.style.marginBottom = "40px";
-  //   });
 }
 
 paintTweets();
