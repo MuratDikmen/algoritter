d179d06c7c54cec26d922b8396e7999daaad9a90
Murat Dikmen <mrtdkmn@gmail.com>
2019-12-05 14:44:45 -0500

    wip: capturing tweets from DOM

--- getTweets.js
+++ getTweets.js
@@ -1,91 +1,210 @@
 let tweets = [];
+let tweetContents = [];
+let counter = 1;
 
 const mutationObserver = new MutationObserver(mutations => {
   mutations.forEach(mutation => {
-    if (
-      mutation.addedNodes.length > 0 &&
-      mutation.target.nodeName == "DIV" &&
-      mutation.target.classList.value === "" &&
-      mutation.addedNodes[0].nodeName == "DIV"
-    ) {
-      // if (!tweets.includes(mutation.addedNodes[0].textContent)) {
-      //   tweets.push(mutation.addedNodes[0].textContent);
-      //   console.log(mutation.addedNodes[0].textContent);
-      // } else {
-      //   console.log("Duplicate found...");
-      // }
-      console.log(mutation);
-    }
+    // Determine type of mutation e.g.
+    identifyMutation(mutation);
+    // if mutationType == tweet or retweet, do stuff.
+    // this will keep the code clean.
 
-    // Original Tweet ID
-    // mutation.addedNodes[0].childNodes[0].childNodes[0].childNodes[0].childNodes[1].childNodes[1].childNodes[0].childNodes[0].childNodes[2].href
-
-    // Reweet ID (Tweet Inside Retweet)
-    // mutation.addedNodes[0].childNodes[0].childNodes[0].childNodes[0].childNodes[1].childNodes[1].childNodes[0].childNodes[0].childNodes[2].href
-
-    /* Check for retweets or original tweets
-    if (
-      mutation.addedNodes.length == 1 &&
-      mutation.addedNodes[0].nodeName === "DIV"
-    ) {
-      if (
-        mutation.addedNodes[0].childNodes[0].textContent &&
-        mutation.addedNodes[0].childNodes[0].textContent.includes("Retweeted")
-      ) {
-        mutation.addedNodes[0].style.backgroundColor = "green"; // Recognize tweet.
-        console.log(
-          `This is a retweet. For this one, we will use the retweeted tweet's id to keep track of this tweet in the UI. 
-          However, when calculating the position, we will use the main id_str. 
-          Here is the retweet message: ${mutation.addedNodes[0].childNodes[0].textContent}`
-        );
-      } else if (
-        mutation.addedNodes[0].childNodes[0].childNodes[0].childNodes[0].childNodes[1].childNodes[1].childNodes[0].childNodes[0].childNodes[2].href.includes(
-          "status"
-        )
-      ) {
-        mutation.addedNodes[0].style.backgroundColor = "green"; // Recognize tweet.
-        console.log(
-          `This is an original tweet. This is straight forward. 
-          Here is the string where we\'ll find the id_str: ${mutation.addedNodes[0].childNodes[0].childNodes[0].childNodes[0].childNodes[1].childNodes[1].childNodes[0].childNodes[0].childNodes[2].href}`
-        );
-      } else {
-        console.log("false");
-      }
-    }
-    End checking retweet vs. original tweet*/
+    // Working code below.
+    // if (
+    //   mutation.addedNodes.length > 0 &&
+    //   mutation.target.nodeName == "DIV" &&
+    //   mutation.target.classList.value === "" &&
+    //   mutation.addedNodes[0].nodeName == "DIV"
+    // ) {
+    //   if (
+    //     mutation.addedNodes[0].childNodes[0].childNodes[0].childNodes[0].childNodes[1].childNodes[1].childNodes[0].childNodes[0].childNodes[2].href.includes(
+    //       "status"
+    //     ) &&
+    //     !tweets.includes(
+    //       mutation.addedNodes[0].childNodes[0].childNodes[0].childNodes[0]
+    //         .childNodes[1].childNodes[1].childNodes[0].childNodes[0]
+    //         .childNodes[2].href
+    //     )
+    //   ) {
+    //     tweets.push(
+    //       mutation.addedNodes[0].childNodes[0].childNodes[0].childNodes[0]
+    //         .childNodes[1].childNodes[1].childNodes[0].childNodes[0]
+    //         .childNodes[2].href
+    //     );
+    //     const div = document.createElement("div");
+    //     div.textContent = tweets.indexOf(
+    //       mutation.addedNodes[0].childNodes[0].childNodes[0].childNodes[0]
+    //         .childNodes[1].childNodes[1].childNodes[0].childNodes[0]
+    //         .childNodes[2].href
+    //     );
+    //     div.style.backgroundColor = "blue";
+    //     div.style.color = "white !important";
+    //     mutation.addedNodes[0].childNodes[0].childNodes[0].childNodes[0].childNodes[1].childNodes[1].childNodes[0].childNodes[0].appendChild(
+    //       div
+    //     );
+    //   } else {
+    //     const div = document.createElement("div");
+    //     div.textContent = tweets.indexOf(
+    //       mutation.addedNodes[0].childNodes[0].childNodes[0].childNodes[0]
+    //         .childNodes[1].childNodes[1].childNodes[0].childNodes[0]
+    //         .childNodes[2].href
+    //     );
+    //     div.style.backgroundColor = "blue";
+    //     div.style.color = "white !important";
+    //     mutation.addedNodes[0].childNodes[0].childNodes[0].childNodes[0].childNodes[1].childNodes[1].childNodes[0].childNodes[0].appendChild(
+    //       div
+    //     );
+    //   }
+    // }
+    // Working code above.
   });
 });
 
 mutationObserver.observe(document.documentElement, {
-  attributes: true,
-  characterData: true,
-  childList: true,
   subtree: true,
-  attributeOldValue: true,
-  characterDataOldValue: true
+  childList: true
 });
-
-// Painting captures tweets -- In the future should include numbering, once the capture method is finalized.
-// function paintTweets() {
-//   let articles;
-//   articles = document.querySelectorAll("article");
-//   let articleArr = Array.from(articles);
-//   console.log(articleArr.length);
-//   console.log(articleArr);
-//   articleArr[0].style.backgroundColor = "blue";
-// }
-
-// paintTweets();
 
 // This method works, the only issue is textContent changes -- The time is updated, therefore the entire textContent is different.
 function getinitialTweets() {
-  const articles = document.querySelectorAll("article");
-  articles.forEach(article => {
-    const tweetText = article.parentNode.parentNode.textContent;
-    tweets.push(tweetText);
-    console.log(tweetText);
+  const div = document.querySelector(
+    "div[aria-label='Timeline: Your Home Timeline']"
+  );
+  const initialTweets = div.childNodes[0].childNodes[0].childNodes;
+  initialTweets.forEach(tweet => {
+    if (tweet.textContent !== "") {
+      const href =
+        tweet.lastChild.childNodes[0].childNodes[0].childNodes[1].lastChild
+          .firstChild.firstChild.lastChild.href;
+      // const tweetContent = tweet.textContent;
+      // const tweetContentLastChars = tweetContent.substring(
+      //   tweetContent.length - 25,
+      //   tweetContent.length
+      // );
+
+      if (!tweets.includes(href)) {
+        tweets.push(href);
+        console.log(href);
+      }
+
+      console.log(tweets.length);
+
+      const num = tweets.indexOf(href);
+
+      const p = document.createElement("p");
+      p.style.width = "30px";
+      p.style.height = "30px";
+      p.style.backgroundColor = "rgb(220, 54, 126)";
+      p.style.borderRadius = "50%";
+      p.style.display = "flex";
+      p.style.alignItems = "center";
+      p.style.justifyContent = "center";
+      p.style.fontSize = "20px";
+      p.style.color = "white";
+      p.style.textAlign = "center";
+      p.style.fontFamily = "sans-serif";
+      p.textContent = String(num);
+      // counter++;
+      tweet.insertBefore(p, tweet.firstChild);
+      // console.log(tweets);
+      // console.log(tweetContents);
+    }
   });
 }
 
 // For now disabled
-// getinitialTweets();
+
+// UI Pattern
+// Create UI object that returns all methods.
+// Finally const ui = new UI()
+// ui.observe(args)
+
+const identifyMutation = mutation => {
+  let mutationType = "";
+  // Check mutation type
+  // First check: mutation.addedNodes must be equal to greater than 0
+  if (
+    mutation.addedNodes.length > 0 &&
+    mutation.target.nodeName == "DIV" &&
+    mutation.target.classList.value === "" &&
+    mutation.addedNodes[0].nodeName == "DIV" &&
+    mutation.addedNodes[0].textContent != "" &&
+    !mutation.addedNodes[0].textContent.includes("Who to Follow") &&
+    mutation.addedNodes[0].childNodes[0].childNodes[0].nodeName == "ARTICLE"
+  ) {
+    // This certainly captures all tweet objects. But does it only capture tweet objects?
+    //// First, Check if Captured, If not proceed with the Identification phase.. Because childNodes etc. might be different depending on
+    //// Scrolling up or down
+    let isCaptured = false;
+
+    // console.log(
+    //   mutation.addedNodes[0].childNodes[0].childNodes[0].childNodes[0]
+    //     .childNodes[0].textContent
+    // );
+
+    const href =
+      mutation.addedNodes[0].lastChild.childNodes[0].childNodes[0].childNodes[1]
+        .lastChild.firstChild.firstChild.lastChild.href;
+
+    // Check
+
+    //
+
+    if (!isCaptured) {
+      // Now capture the tweet...
+    }
+
+    let isThread = false;
+    let isPreviousThread = false;
+
+    // if (mutation.addedNodes[0].innerHTML.includes("r-432wen")) {
+    //   isThread = true;
+    //   if (
+    //     mutation.addedNodes[0].previousSibling.innerHTML.includes("r-432wen")
+    //   ) {
+    //     isPreviousThread = true;
+    //   }
+    // }
+
+    const tweetContent = mutation.addedNodes[0].textContent;
+    const tweetContentLastChars = tweetContent.substring(
+      tweetContent.length - 25,
+      tweetContent.length
+    );
+
+    if (!tweets.includes(href)) {
+      tweets.push(href);
+    }
+
+    const num = tweets.indexOf(href);
+
+    const p = document.createElement("p");
+    p.style.width = "30px";
+    p.style.height = "30px";
+    p.style.backgroundColor = "rgb(220, 54, 126)";
+    p.style.borderRadius = "50%";
+    p.style.display = "flex";
+    p.style.alignItems = "center";
+    p.style.justifyContent = "center";
+    p.style.fontSize = "20px";
+    p.style.color = "white";
+    p.style.textAlign = "center";
+    p.style.fontFamily = "sans-serif";
+    p.textContent = String(num);
+    if (isThread) {
+      p.style.backgroundColor = "blue";
+    }
+    if (isPreviousThread) {
+      p.style.backgroundColor = "red";
+    }
+    // counter++;
+    mutation.addedNodes[0].insertBefore(p, mutation.addedNodes[0].firstChild);
+    // console.log(tweets.length);
+    // console.log(tweetContents);
+    // console.log(mutation);
+  }
+
+  // return mutationType;
+};
+
+getinitialTweets();
