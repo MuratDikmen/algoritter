6622e1ec8b96f063452642ace5ab29193b817edb
Murat Dikmen <mrtdkmn@gmail.com>
2020-10-21 17:16:05 -0400

    major refactoring

--- getTweets.js
+++ getTweets.js
@@ -1,37 +1,22 @@
+// =========================
+// DECLARE GLOBAL VARIABLES
+// =========================
 let tweets = [];
-let tweetContents = [];
-let counter = 1;
 let tweetsToCompare = [];
 
+// ==================================
+// SET UP MUTATION OBSERVER FUNCTION
+// ==================================
 const mutationObserver = new MutationObserver((mutations) => {
   mutations.forEach((mutation) => {
     handleMutation(mutation);
   });
 });
 
-// ==============================
+// ====================================================
 // IDENTIFY EACH MUTATION AND PROCESS IF IT IS A TWEET
-// ==============================
+// ====================================================
 const handleMutation = (mutation) => {
-  if (
-    mutation.addedNodes.length > 0 &&
-    mutation.addedNodes[0].tagName === "SECTION" &&
-    mutation.addedNodes[0].lastChild.ariaLabel ===
-      "Timeline: Your Home Timeline"
-  ) {
-    console.log(mutation);
-  }
-
-  // Check if the tweets are initial tweets - the difference from regular tweets is
-  // that the first couple tweets are added together as a section.
-  // Therefore, mutationHandler should catch if the addedNode is a section and parse href accordingly.
-
-  // if (mutation.addedNodes.length > 0 &&) {
-
-  // }
-
-  // mutation.addedNodes.length > 0 && console.log(mutation);
-
   // Check to see if the added node is the div that contains the tweet.
   // Note that this method is very fragile and doesn't work if Twitter changes the DOM structure.
   if (
@@ -41,55 +26,29 @@ const handleMutation = (mutation) => {
     mutation.addedNodes[0].tagName == "DIV" &&
     mutation.addedNodes[0].textContent != "" &&
     !mutation.addedNodes[0].textContent.includes("Who to Follow") &&
-    mutation.addedNodes[0].childNodes[0].childNodes[0].childNodes[0]
-      .childNodes[0].firstChild.tagName == "ARTICLE"
+    mutation.addedNodes[0].childNodes[0].childNodes[0].childNodes[0].tagName == "ARTICLE"
   ) {
-    // Defining the link in steps, otherwise it is very difficult to troubleshoot when Twitter changes the DOM structure.
+    // Getting the link (to Twitter handle) step by step, otherwise it is very
+    // difficult to troubleshoot when Twitter changes the DOM structure.
     let div = mutation.addedNodes[0];
-    let article =
-      div.childNodes[0].childNodes[0].childNodes[0].childNodes[0].firstChild;
+    let article = div.childNodes[0].childNodes[0].childNodes[0].childNodes[0].firstChild;
     let tweetColumn = article.firstChild.lastChild.lastChild;
     let upperBar = tweetColumn.firstChild.firstChild.firstChild;
     let link = upperBar.firstChild.lastChild;
 
-    console.log(mutation);
-
-    // Get the tweet handle link.
-    const href = link.href;
-
-    // let isThread = false;
-    // let isPreviousThread = false;
-    // let isRetweet = false;
-
-    // if (
-    //   mutation.addedNodes[0].lastChild.firstChild.firstChild.firstChild.textContent.includes(
-    //     "Retweeted"
-    //   )
-    // ) {
-    //   isRetweet = true;
-    // }
-
     // Return if the tweet is part of a thread -- Need to find a cleaner way of implementing this.
     if (
-      article.firstChild.lastChild.firstChild.lastChild.classList.contains(
-        "r-m5arl1"
-      ) ||
-      (article.firstChild.firstChild.firstChild.firstChild.childNodes.length >
-        0 &&
-        article.firstChild.firstChild.firstChild.firstChild.firstChild.classList.contains(
-          "r-m5arl1"
-        ))
+      article.firstChild.lastChild.firstChild.lastChild.classList.contains("r-m5arl1") ||
+      (article.firstChild.firstChild.firstChild.firstChild.childNodes.length > 0 &&
+        article.firstChild.firstChild.firstChild.firstChild.firstChild.classList.contains("r-m5arl1"))
     ) {
       return;
     }
 
-    // const tweetContent = mutation.addedNodes[0].textContent;
-    // const tweetContentLastChars = tweetContent.substring(
-    //   tweetContent.length - 25,
-    //   tweetContent.length
-    // );
+    // Get the tweet handle link.
+    const href = link.href;
 
-    // Get tweet handle from the URL.
+    // Get the tweet handle from the URL.
     const tweetId = href.split("/").pop();
 
     // Add tweet handle to the list of tweets to keep track of.
@@ -100,8 +59,7 @@ const handleMutation = (mutation) => {
     // Get indices of the tweet in client tweet list and server tweet list and get the difference in ranks.
     const onScreenPosition = tweets.indexOf(tweetId);
     const originalPosition = tweetsToCompare.indexOf(tweetId);
-    const rankChange =
-      originalPosition == -1 ? 999 : originalPosition - onScreenPosition;
+    const rankChange = originalPosition == -1 ? 999 : originalPosition - onScreenPosition;
 
     // Initialize a new UI object and create an icon based on the rank differences between client and server.
     const ui = new UI();
@@ -109,53 +67,15 @@ const handleMutation = (mutation) => {
 
     // Add icon next to the Tweet title.
     link.parentNode.appendChild(icon);
-  } else {
-    console.log("This is not a Tweet / Failed to catch the Tweet");
   }
 };
 
-// ==============================
-// Initialization of the App
-// ==============================
-async function init() {
-  try {
-    console.log("script is running.");
-
-    const res = await fetch("https://algoritter.herokuapp.com/");
-    const data = await res.json();
-    let threadFreeTweetList = [];
-    let tweetsToRemove = [];
-    data.data.forEach((tweet) => {
-      if (tweet.in_reply_to_status_id_str !== null) {
-        tweetsToRemove.push(tweet.in_reply_to_status_id_str);
-        tweetsToRemove.push(tweet.id_str);
-      }
-    });
-
-    threadFreeTweetList = data.data.filter(
-      (tweet) => !tweetsToRemove.includes(tweet.id_str)
-    );
-    tweetsToCompare = threadFreeTweetList.map((tweet) =>
-      tweet.retweeted_status == null
-        ? tweet.id_str
-        : tweet.retweeted_status.id_str
-    );
-
-    // getinitialTweets();
-
-    mutationObserver.observe(document.documentElement, {
-      subtree: true,
-      childList: true,
-    });
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-init();
-
+// ===================================
+// SET UP UI FUNCTION TO HANDLE ICONS
+// ===================================
 function UI() {
   const createRankIcon = (rankDiff) => {
+    // Create icon template
     const div = document.createElement("div");
     const arrow = document.createElement("div");
     div.style.fontFamily = "Arial";
@@ -169,23 +89,28 @@ function UI() {
     arrow.style.borderWidth = "10px";
 
     if (rankDiff === 999) {
+      // Tweet is not in the original tweet list
+      // Could be a tweet from a long time ago or a liked tweet
       arrow.style.border = "10px solid purple";
       arrow.style.transform = "translate(0%, 20%) scale(.8)";
       arrow.style.borderRadius = "50%";
       div.appendChild(arrow);
     } else if (rankDiff > 0) {
+      // Tweet is promoted by Twitter AI
       arrow.style.borderBottom = "10px solid #00c853";
       arrow.style.transform = "translate(5%, -5%)";
       div.style.color = "#00c853";
       div.appendChild(arrow);
       div.appendChild(document.createTextNode(`+${rankDiff}`));
     } else if (rankDiff < 0) {
+      // Tweet is demoted by Twitter AI
       arrow.style.transform = "translate(0%, 50%) rotate(0deg)";
       arrow.style.borderTop = "10px solid #dd2c00";
       div.style.color = "#dd2c00";
       div.appendChild(arrow);
       div.appendChild(document.createTextNode(`${rankDiff}`));
     } else {
+      // Tweet is neither promoted nor demoted by Twitter AI (same rank)
       arrow.style.border = "10px solid yellow";
       arrow.style.transform = "translate(0%, 25%) scale(.7, .2)";
       div.style.color = "yellow";
@@ -199,3 +124,46 @@ function UI() {
     createRankIcon: createRankIcon,
   };
 }
+
+// ===================
+// APP INITIALIZATION
+// ===================
+async function init() {
+  try {
+    console.log("script is running.");
+
+    // Get data from the server
+    const res = await fetch("https://algoritter.herokuapp.com/");
+    const data = await res.json();
+
+    // Identify threads and remove them from the data
+    let threadFreeTweetList = [];
+    let tweetsToRemove = [];
+    data.data.forEach((tweet) => {
+      if (tweet.in_reply_to_status_id_str !== null) {
+        tweetsToRemove.push(tweet.in_reply_to_status_id_str);
+        tweetsToRemove.push(tweet.id_str);
+      }
+    });
+    threadFreeTweetList = data.data.filter((tweet) => !tweetsToRemove.includes(tweet.id_str));
+
+    // Create the tweet list by getting the correct tweet handle
+    // Get tweet handle for the retweeter for retweets, otherwise regular tweet handle
+    tweetsToCompare = threadFreeTweetList.map((tweet) =>
+      tweet.retweeted_status == null ? tweet.id_str : tweet.retweeted_status.id_str
+    );
+
+    // Start observing the mutations
+    mutationObserver.observe(document.documentElement, {
+      subtree: true,
+      childList: true,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// ===================================
+// INIT APP
+// ===================================
+init();
